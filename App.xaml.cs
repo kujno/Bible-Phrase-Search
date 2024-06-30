e78941449b187c47876902a780ee4a8b42dc06eb
Matej Kujnisch <matej.kujnisch@gmail.com>
2024-06-30 21:32:04 +0200

    first commit

---
+++ App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Bible_Word_Finder
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Bible_Word_Finder
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void menu_open_click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void menu_exit_click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void menu_about_click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
